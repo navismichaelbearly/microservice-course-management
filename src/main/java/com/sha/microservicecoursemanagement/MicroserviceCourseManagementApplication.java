30c047744ba9b9be6e6ef1bada577e892e5a46ef
NBEARLYJ <nbearlyj@ford.com>
2023-03-06 14:49:43 +0530

    added with course details

--- src/main/java/com/sha/microservicecoursemanagement/MicroserviceCourseManagementApplication.java
+++ src/main/java/com/sha/microservicecoursemanagement/MicroserviceCourseManagementApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,4 +17,13 @@ public class MicroserviceCourseManagementApplication {
 		SpringApplication.run(MicroserviceCourseManagementApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
